ca4cf7b776706cd7c1385a2374e1ef7f476a33e3
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-05-11 12:41:54 +0700

    push operation

--- stack_using_linked_list.cpp
+++ stack_using_linked_list.cpp
@@ -22,6 +22,16 @@ public:
 		top = NULL; // initialize the stack with a null top pointer
 	}
 
+	// push operation: insert an element into the top of the stack
+	int push(int value) {
+		Node* newNode = new Node();// 1. Allocate memory for the new node
+		newNode->data = value; // 2. assign value
+		newNode->next = top; // 3. set the next pointer of the new node to current top node
+		top = newNode; // 4. Update the top pointer to the new node
+		cout << "Push value: " << value << endl;
+		return value;
+	}
+
 	
 
 };
